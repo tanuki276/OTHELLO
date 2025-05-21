ab334a18eddbc7bb4e801126066fc5aa7fad713d
blowa <light06120602@gmail.com>
2025-05-21 18:10:09 +0900

    pw.js の作成

---
+++ pw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'othello-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icons/android-icon-192x192.png',
+  '/icons/favicon-16x16.png',
+];
+
+// インストール時に必要なファイルをキャッシュ
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// フェッチ時はキャッシュ優先で返す（オフライン対応）
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// 新しいサービスワーカーがアクティブになったら古いキャッシュを削除
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+});
